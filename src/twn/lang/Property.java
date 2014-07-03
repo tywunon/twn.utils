eaaa0b7f396a7eac77c87ecdcfef1ef80f6728b0
tywunon <tywunon@gmx.de>
2014-07-03 12:40:09 +0200

    - Added automatic Property eventHandling

--- src/twn/lang/Property.java
+++ src/twn/lang/Property.java
@@ -1,14 +1,19 @@
 package twn.lang;
 
+import java.lang.reflect.Field;
+import java.util.Set;
+
 import twn.evt.Event;
 import twn.evt.EventArgs;
+import twn.util.Cast;
+import twn.util.Clazz;
 
 public class Property<T> {
 	private T value;
 	private final Object eventOwner = new Object();
 	public final Event<PropertyChangedEventArgs<T>> propertyChanged = new Event<>(eventOwner);
 	
-	public Property(){}
+	public Property() {}
 	
 	public Property (T initValue) {
 		set(initValue);
@@ -36,4 +41,88 @@ public class Property<T> {
 			this.oldValue = oldValue;
 		}
 	}
+	
+	public static abstract class PropertyContainer {
+		
+		private final Object eventOwner;
+		public final Event<PropertyContainerChangedArgs> containerChanged;
+		
+		protected PropertyContainer() {
+			eventOwner = new Object();
+			containerChanged = new Event<>(eventOwner);
+		}
+		
+		protected final void initPropertys() {
+			Set<Field> fields = Clazz.getAllFields(this.getClass());
+			fields.stream()
+			.filter(
+					(f) -> f.getType().equals(Property.class)
+					)
+			.forEach(
+					(f) -> {
+						boolean accessible = f.isAccessible();
+						try {
+							f.setAccessible(true);
+							Property<?> prop = Cast.as(f.get(this), Property.class);
+							if(prop != null) {
+								prop.propertyChanged.subscribe(this::propertyChanged);
+							}
+							f.setAccessible(accessible);
+						} catch (Exception e) {
+							e.printStackTrace();
+						} finally {
+							f.setAccessible(accessible);
+						}
+					}
+			);
+		}
+		
+		protected final void propertyChanged(Object sender, PropertyChangedEventArgs<?> args) {
+			Clazz.getAllFields(this.getClass()).stream()
+			.filter(
+				(f) -> {
+					boolean accessible = f.isAccessible();
+					boolean result = false;
+					try {
+						f.setAccessible(true);
+						Object obj = f.get(this);
+						result = obj == sender;
+					} catch (Exception e) {
+						e.printStackTrace();
+					} finally {
+						f.setAccessible(accessible);
+					}
+					return result;
+				}
+			)
+			.forEach(
+				(f) -> {
+					raiseModelObjectChanged(f.getName(), args.oldValue, args.newValue);
+				}
+			);
+		}
+		
+		protected final <T> void raiseModelObjectChanged(String propertyName, T oldValue, T newValue){
+			containerChanged.fire(eventOwner, this, new PropertyContainerChangedArgs(this, propertyName, newValue.getClass(), oldValue, newValue));
+		}
+	}
+	
+	public static class PropertyContainerChangedArgs extends EventArgs{
+		public final PropertyContainer container;
+		public final String property;
+		public final Class<?> valueType;
+		public final Object oldValue;
+		public final Object newValue;
+		
+		public PropertyContainerChangedArgs(PropertyContainer container, String property, Class<?> valueType, Object oldValue, Object newValue) {
+			super(false);
+			this.container = container;
+			this.property = property;
+			this.valueType = valueType;
+			this.oldValue = oldValue;
+			this.newValue = newValue;
+		}
+	}
+	
+	
 }
--- src/twn/test/LangTest.java
+++ src/twn/test/LangTest.java
@@ -4,20 +4,22 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 import twn.lang.Property;
+import twn.lang.Property.PropertyContainer;
+import twn.lang.Property.PropertyContainerChangedArgs;
 import twn.lang.SetOnce;
 
-public class UtilTest {
+public class LangTest {
 
 	boolean expectEvent;
+	boolean eventRecieved;
 	
 	@Test
-	public void testProperty() {
-
+	public void testPropertyGetSet() {
+		expectEvent = false;
 		Property<String> string = new Property<String>();
 		assertEquals("null without initValue", null, string.get());
 		string = new Property<>("Hello");
 		assertEquals("InitValue?", "Hello", string.get());
-		expectEvent = false;
 		string.propertyChanged.subscribe((sender, args) -> assertTrue(expectEvent));
 		string.set("Hello");
 		assertEquals("still InitValue?", "Hello", string.get());
@@ -25,6 +27,26 @@ public class UtilTest {
 		string.set("Test");
 		assertEquals("New Value", "Test", string.get());
 	}
+	
+	@Test
+	public void testPropertyChanged() {
+		expectEvent = false;
+		eventRecieved = false;
+		TestPropertyContainer tpc = new TestPropertyContainer();
+		tpc.containerChanged.subscribe(this::handleContainerChanged);
+		expectEvent = true;
+		tpc.string.set("settet");
+		assertTrue(eventRecieved);
+		
+	}
+	
+	public void handleContainerChanged(Object sender, PropertyContainerChangedArgs args) {
+		eventRecieved = true; 
+		assertTrue(expectEvent);
+		assertEquals("init", args.oldValue); 
+		assertEquals("settet", args.newValue); 
+	}
+	
 	
 	@Test
 	public void testSetOnce() {
@@ -38,5 +60,13 @@ public class UtilTest {
 		assertEquals("expect initValue", "Hello", string.value());
 		assertTrue("is Set", string.isSet());
 	}
-
+	
+	private static class TestPropertyContainer extends PropertyContainer {
+		public final Property<String> string;
+		
+		public TestPropertyContainer() {
+			string = new Property<String>("init");
+			initPropertys();
+		}
+	}
 }
